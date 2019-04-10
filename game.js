186a19d4eae33b9ca2563793ce79ec3afc942466
Bartosz Kamiński <bakaminski@wp.pl>
2019-04-10 20:58:34 +0200

    add game.html with css and js

---
+++ game.js
@@ -0,0 +1,260 @@
+// first letter to upperacase
+String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+// variables
+const womanNamePlace = document.querySelector(".womanNamePlace")
+const manNamePlace = document.querySelector(".manNamePlace")
+const womanName = sessionStorage.getItem('womanName').capitalize()
+const manName = sessionStorage.getItem('manName').capitalize()
+const womanMission = sessionStorage.getItem('womanTask').capitalize()
+const manMission = sessionStorage.getItem('manTask').capitalize()
+let womanPoints = 0
+let manPoints = 0
+let numberManDice = "";
+let numberWomanDice = "";
+let diceCounter = "";
+let moveMan = 0
+let moveWoman = 0
+let roundWoman = 0
+let roundMan = 0
+const manDiceButton = '<button class="drawManBtn btn" onclick="throwManDice()">Rzuć kostką</button>';
+const womanDiceButton = '<button class="drawWomanBtn btn" onclick="throwWomanDice()">Rzuć kostką</button>';
+const doneButton = '<button class="doneBtn" onclick="done()">Wykonane</button>';
+const failButton = '<button class="failBtn" onclick="failed()">Niewykonane</button>';
+
+// add names form prev pages
+function getNames() {
+    womanNamePlace.textContent = womanName
+    manNamePlace.textContent = manName
+}
+
+function getPoints() {
+    document.querySelector(".womanPoints").innerHTML = `<h3>Punkty: ${womanPoints}<h3>`
+    document.querySelector(".manPoints").innerHTML = `<h3>Punkty: ${manPoints}</h3>`
+}
+getNames()
+getPoints()
+
+// change color of points
+changePointsColor = () => {
+    if (manPoints > womanPoints) {
+        document.querySelector(".manPoints").style.color = "green"
+        document.querySelector(".womanPoints").style.color = "red"
+    } else if (manPoints < womanPoints) {
+        document.querySelector(".manPoints").style.color = "red"
+        document.querySelector(".womanPoints").style.color = "green"
+    } else {
+        document.querySelector(".manPoints").style.color = "white"
+        document.querySelector(".womanPoints").style.color = "white"
+    }
+}
+// throw dice's woman and get dice's number
+throwWomanDice = () => {
+    if (moveWoman == 1 && moveMan == 0) {
+        numberWomanDice = Math.floor(Math.random() * 3) + 1;
+        let dice = "<img src=\"img/dice" + numberWomanDice + ".jpg\"/>";
+        document.getElementById("diceW").innerHTML = "";
+        document.getElementById("diceW").innerHTML = dice;
+        diceCounter++
+    } else if (moveMan == 1 && moveWoman == 0) {
+        numberWomanDice = Math.floor(Math.random() * 3) + 4;
+        let dice = "<img src=\"img/dice" + numberWomanDice + ".jpg\"/>";
+        document.getElementById("diceW").innerHTML = "";
+        document.getElementById("diceW").innerHTML = dice;
+        diceCounter++
+    } else if (moveWoman == 4 && moveMan < moveWoman) {
+        numberWomanDice = Math.floor(Math.random() * 3) + 1;
+        let dice = "<img src=\"img/dice" + numberWomanDice + ".jpg\"/>";
+        document.getElementById("diceW").innerHTML = "";
+        document.getElementById("diceW").innerHTML = dice;
+        diceCounter++
+    } else {
+        numberWomanDice = Math.floor(Math.random() * 6) + 1;
+        let dice = "<img src=\"img/dice" + numberWomanDice + ".jpg\"/>";
+        document.getElementById("diceW").innerHTML = "";
+        document.getElementById("diceW").innerHTML = dice;
+        diceCounter++
+
+    }
+    if (diceCounter === 2) {
+        chooseTask();
+    }
+}
+// throw man dices and get dices number
+throwManDice = () => {
+    if (moveWoman == 1 && moveMan == 0) {
+        numberManDice = Math.floor(Math.random() * 3) + 4;
+        let dice = "<img src=\"img/dice" + numberManDice + ".jpg\"/>";
+        document.getElementById("diceM").innerHTML = "";
+        document.getElementById("diceM").innerHTML = dice;
+        diceCounter++
+    } else if (moveMan == 1 && moveWoman == 0) {
+        numberManDice = Math.floor(Math.random() * 3) + 1
+        let dice = "<img src=\"img/dice" + numberManDice + ".jpg\"/>";
+        document.getElementById("diceM").innerHTML = "";
+        document.getElementById("diceM").innerHTML = dice;
+        diceCounter++
+    } else if (moveMan == 4 && moveMan > moveWoman) {
+        numberManDice = Math.floor(Math.random() * 3) + 1
+        let dice = "<img src=\"img/dice" + numberManDice + ".jpg\"/>";
+        document.getElementById("diceM").innerHTML = "";
+        document.getElementById("diceM").innerHTML = dice;
+        diceCounter++
+    } else {
+        numberManDice = Math.floor(Math.random() * 6) + 1;
+        let dice = "<img src=\"img/dice" + numberManDice + ".jpg\"/>";
+        document.getElementById("diceM").innerHTML = "";
+        document.getElementById("diceM").innerHTML = dice;
+        diceCounter++
+    }
+    if (diceCounter === 2) {
+        chooseTask();
+    }
+}
+
+document.querySelector('.drawWomanBtn').addEventListener("click", throwWomanDice)
+document.querySelector('.drawManBtn').addEventListener("click", throwManDice)
+// hide thrown buttons
+hideThrowBtns = () => {
+    $("#diceM").fadeOut(1250);
+    $("#diceW").fadeOut(1250);
+}
+// show dices after click done or fail button
+showDice = () => {
+    $("#diceM").show(700);
+    $("#diceW").show(700);
+}
+// choosing task to player
+chooseTask = () => {
+    if (numberWomanDice > numberManDice) {
+        moveWoman++
+        hideThrowBtns()
+        numberTask = Math.floor(Math.random() * tasksWoman[moveWoman - 1].length)
+        let drawnTask = tasksWoman[moveWoman - 1][numberTask]
+        document.getElementById("task").innerHTML = drawnTask
+        showDoneFailedBtns()
+        showTask()
+    } else if (numberWomanDice < numberManDice) {
+        moveMan++
+        hideThrowBtns()
+        numberTask = Math.floor(Math.random() * tasksMan[moveMan - 1].length)
+        let drawnTask = tasksMan[moveMan - 1][numberTask]
+        document.getElementById("task").innerHTML = drawnTask
+        showDoneFailedBtns()
+        showTask()
+    } else {
+        numberWomanDice = ""
+        numberManDice = ""
+        diceCounter = 0
+        setTimeout(function () {
+            document.getElementById('diceW').innerHTML = womanDiceButton
+        }, 1000);
+        setTimeout(function () {
+            document.getElementById('diceM').innerHTML = manDiceButton
+        }, 1000);
+    }
+}
+
+
+showTask = () => {
+    $(document).ready(function () {
+        $('#task').delay(1350).show(1550);
+    });
+}
+
+showDoneFailedBtns = () => {
+    setTimeout(function () {
+        document.getElementById("done").innerHTML = doneButton
+        document.getElementById("fail").innerHTML = failButton
+    }, 2800);
+}
+
+checkWhoWin = () => {
+    if (manPoints > womanPoints) {
+        document.getElementById("task").innerHTML = `<h1>Gratulacje!</h1> <h2>Wygrał ${manName}</h2>  <h3>${womanName} pamiętaj o zadaniu, które musisz spełnić:</h3> <p>${manMission} </p>`
+    } else if (manPoints < womanPoints) {
+        document.getElementById("task").innerHTML = `<h1>Gratulacje!</h1> <h2>Wygrała ${womanName}</h2>  <h3>${manName} pamiętaj o zadaniu, które musisz spełnić: </h3> <p>${womanMission} </p>`
+    }
+}
+
+resetAfterClick = () => {
+    document.getElementById("done").innerHTML = " ";
+    document.getElementById("fail").innerHTML = " ";
+    if (moveMan === 6 || moveWoman === 6) {
+        checkWhoWin()
+    } else {
+        document.getElementById("diceW").innerHTML = womanDiceButton;
+        document.getElementById("diceM").innerHTML = manDiceButton;
+        task.style.display = "none";
+        diceWoman = "";
+        diceMan = "";
+        diceCounter = "";
+        showDice()
+
+    }
+}
+
+done = () => {
+    if (numberManDice > numberWomanDice) {
+        manPoints = manPoints + moveMan
+        getPoints()
+        greenCircleMan()
+        resetAfterClick()
+    } else if (numberManDice < numberWomanDice) {
+        womanPoints = womanPoints + moveWoman
+        getPoints()
+        greenCircleWoman()
+        resetAfterClick()
+    }
+    changePointsColor()
+}
+
+failed = () => {
+    if (numberManDice > numberWomanDice) {
+        manPoints = manPoints - moveMan
+        getPoints()
+        redCircleMan()
+        resetAfterClick()
+    }
+    if (numberManDice < numberWomanDice) {
+        womanPoints = womanPoints - moveWoman
+        redCircleWoman()
+        getPoints()
+        resetAfterClick()
+    }
+    changePointsColor()
+}
+
+
+greenCircleMan = () => {
+    for (i = 0; i <= 6; i++)
+        if (moveMan === i) {
+            let number = "cM" + i
+            document.getElementById(number).classList.add("green")
+        }
+}
+
+redCircleMan = () => {
+    for (i = 0; i <= 6; i++)
+        if (moveMan === i) {
+            let number = "cM" + i
+            document.getElementById(number).classList.add("red")
+        }
+}
+
+greenCircleWoman = () => {
+    for (i = 0; i <= 6; i++)
+        if (moveWoman === i) {
+            let number = "cW" + i
+            document.getElementById(number).classList.add("green")
+        }
+}
+
+redCircleWoman = () => {
+    for (i = 0; i <= 6; i++)
+        if (moveWoman === i) {
+            let number = "cW" + i
+            document.getElementById(number).classList.add("red")
+        }
+}
---
+++ tasks.js
@@ -0,0 +1,193 @@
+const tasksWoman = [];
+tasksWoman[0] = [
+    `${womanName}, usiądź seksownie na kolanach Partnera i całuj go przez dwie minuty z języczkiem. Pod koniec sciągnij z niego odzienie i zostaw go w samej bieliznie. ${manName} rączki przy sobie! ${womanName} przyjmuję inicjatywe i ma nad tobą pełną kontrolę.`,
+
+    `${womanName}, jesteś wynajętą eksluzywną striptizerką. Wykonaj seksowny pięcio minutowy striptiz przed swoim bogatym Klientem. Masz go kusić, podniecić i rozebrać do bielizny. ${manName}, niestety nie możesz dotykać tej seksownej Stripizerki, Ona przyjmuję inicjatywe.`,
+
+    ` ${womanName}, seksownie zdejmij odzienie Partnera do samej bielizny,a następnie wycałuj jego ciałko ;)`,
+
+    `${womanName}, widzisz jak Partner niekomfortowo czuje się w ubraniach? Zdejmij je i zostaw go w samej bieliźnie. ${womanName}, zacznij od dolnej części ciała. W czasie sciągania ubrań, dotykaj namiętnie  miejsca intymne, ale tylko przez bielizne.`,
+
+    `${womanName}, ten meżczyzna obok Ciebie był niegrzeczny! Zdejmij mu ubrania z górnej częsci ciała, a następnie spodnie. Każ mu wypiąc dupkę i daj mu 3 klapsy. `,
+
+    ` ${womanName}! Słyszysz to? ${manName} mruczy byś sciągneła z niego te ciuszki, które ma na sobie. Zrób to i zostaw go w samej blieliżnie. Następnie każ mu sie położyć. Wskocz na niego i przez minute seksownie go ujeżdżaj.`,
+]
+
+tasksWoman[1] = [
+    `${womanName}, nasmaruj swoje ciałko oliwka lub balsamem, rób to w taki sposób żeby podniecic partnera.`,
+
+    ` ${womanName}, niech partner położy się na plecach, a ty całuj go po całym ciele, delikatnie chuchajac, możesz przy tym szeptac mu do ucha mile slowka.`,
+
+    `${womanName}, usiądź rozkrakiem na przeciwko Partnera, zacznij piescic swoje czule miejsca do momentu aż zobaczysz, że twój facet jest mega podniecony na ten widok. ${manName} rączki przy sobie, możesz tylko patrzeć i się ślinić.`,
+
+    `${womanName}, polożcie sie obok siebie. Zacznij masować przez majtki członka swojego partnera i jednocześnie liz jego 2 palce tak jakbyś właśnie miała w buzi jego sprzęt.`,
+
+    `${womanName}, Przełóż się przez kolana Partnera i zdejmij delikatnie majtki odsłaniając goły tyłek. Poproś faceta by dał ci 3 klapsy na tyłek.`,
+
+    `${womanName}, każ wstać swojemu Partnerowi, następnie uklęknij i zdejmij jego majtki używając do tego celu tylko swoich ust. Masz wykonać to zadanie w sposób erotyczny. ${manName} od tej pory będzię bez majtek.`,
+
+    `${womanName}, niech twój partner usiądzie wygodnie a ty kleknij przed nim na czworaka i zachowuj się jak kotka która prosi o mleczko. Jeśli będzie bardzo podniecony na ten widok to odsłoni to co ma pod majteczkami a ty będziesz mogła ssać go przez 10 sekund.`,
+
+    `${womanName} usiądź na swoim Partnerze i zacznij się o niego ocierac tak długo aż poczujesz że jest bardzo podniecony.`,
+
+    `${womanName}, usiądź na swoim parterze, całujcie się z języczkiem i jednoczenie namiętnie dotykaj go po włosach i klacie.`,
+
+    `${womanName}, niech partner położy się na plecach, a ty dzięki swoim ustom spraw by jego czule miejsce zadrgało.`,
+
+]
+
+tasksWoman[2] = [
+    `${womanName}, niech Twój Partner położy się na plecach a ty ustami zajmij się jego najczulszymi miejscami, masz na to dwie minuty.`,
+
+    `${womanName}, niech twój Partner wygodnie się położy a ty swoimi stopkami masuj jego członka do momentu aż poczujesz że jest bardzo podniecony.`,
+
+    `${womanName}, zwiaż oczy Partnerowi, niech położy się na plecach, a ty całuj go od góry po sam dół aż dojdziesz do jego członka. Zrób mu dobrze ustami przez trzy minuty.`,
+
+    `${womanName}, każ Partnerowi wziąźć kostkę lodu i pozwól mu drażnić swoje sutki i swój brzuch.`,
+
+    `${womanName}, połóż się na plecach, niech twój partner kleczy nad twoimi ustami. Dalej wiesz co masz robić! Masz na to minute.`,
+
+    `${womanName}, niech twój partner usiądzie na krześle. Twoim zadaniem jest podejście do niego na czworaka i possać mu pytonika przez dwie minuty.`,
+
+]
+
+tasksWoman[3] = [
+    `${womanName}, teraz zasmakujesz smaku swojej muszelki. Połóż się wygodnie i odsłoń szparke. ${manName}, zrob Partnerce dobrze paluszkami przez dwie minuty, a następnie daj jej do oblizania swoje paluszki.`,
+
+    `${womanName}, połóż się na plecach na łóżku, głowe musisz mieć na skraju łóżka. ${manName}, stanie nad Twoją twarzą dokładnie tak jak na poniższym obrazku. Daj mu przez 2 minuty maksimum przyjemności. Sama też możesz się w tym czasie bawić swoją muszelką i dotykać swoich piersi. ${manName} nie możesz dojść. <img src=\"img/pictureW41.jpg\"/>`,
+
+    `${womanName}, poproś Partnera by związał twoje ręce i nogi, a następnie wypnij swoją pupcię i poproś swojego Partnera by dał ci klapsy. Po każdym klapsie proś o więcej aż dostaniesz ich 10. Jeżeli dostaniesz mniej, zadanie zostnie niewykonane.`,
+
+    `${womanName}, właśnie jesteś na castingu do filmu porno. Natomiast Twój Partner jest erotycznym fotografem i masz przyjmować pozy takie jak on każe. ${manName}, weż aparat albo telefon i cyknij parę fotek tej modelce porno i oceń czy się nadaje do filmu.`,
+
+    `${womanName}, własnie jestes w pracy, a ${manName} to twój Szef. Akurat jesteście sami w pomieszczeniu i rozmawiacie o Twoim awansie. Masz przekonać szefa w ciągu 5 minut, że awans się Tobie należy.`,
+
+    `${womanName}, idz nago w miejsce, która jest na widoku, gdzie ktoś może Cie zobaczyć(np. przed okno z odsłoniętymi żaluzjami, balokn itp.), a następnie zrób pokaz swojego ciałą przez 15 sekund. ${manName}, ty możesz się tylko gapić.`,
+]
+
+tasksWoman[4] = [
+    `${manName}, zwiąż swojej Partnerce ręce z tyłu. Następnie usiądź na krześle lub skraju łóżka i każ stwojej partnerce uklęknąć przed Tobą. Wplecz swoją ręke w jej włosy i wsadzaj jak najgłebiej swojego pytonika do jej buzi. ${womanName}, to zadanie polega na twojej uległości. Masz się nie opierać i dać jak najwięcej przyjemności Partnerowi. Masz na 2 minuty.`,
+
+    `Wejdźcie na strone internetową z kamerkami na żywo (np. showup.tv). Możecie tak ustawić kamerke by nie było widać waszych twarzy. Nastepnie ${womanName} ustawi się do kamerki i pokaże cycki lub dupcie przynajmniej kilku widzom.`,
+
+    `${manName}, posadź Partnerke na biurku i zacznij Ja pieprzyć przez 2 minuty. Pamietaj, że nie możesz dojść.`,
+
+    `${manName}, zaprowadź Partnerke na środek podłogi i kochaj się z nią przez 2 minuty. Pamietaj, że nie możesz dojść.`,
+
+    `Wejdźcie na strone internetową z kamerkami na żywo(np. showup.tv). Możecie tak ustawić kamerke by nie było widać waszych twarzy. Następnie ${womanName} pokaże dupcię dla kilku widzów.`,
+
+    `${womanName}, uklęknij przed swoim Partnerem i poproś go, by włożył 3-krotnie swojego Pytona w Twoje usta jak najgłębiej potrafi.`,
+
+    `${womanName}, otwórz wszystkie drzwi i okna w domu. ${manName}, zaczanij się kochać z swoja Partnerką przez 3 minuty w dowolnie wybranej pozycji. Pamiętaj, że nie możesz dojść. ${womanName}, twoim zadaniem jest jak najgłośniejsze wydawanie seksualnych dźwięków podczas kochania się.`,
+
+]
+
+tasksWoman[5] = [
+    `${womanName}, wypnij dupcie dla Partnera przymująć pozycję na pieska. ${manName}, Partnerka wypieła dla Ciebie dupcie. Zacznij się z nią kochać jednocześnie nagrywając to telefonem przez minute.`,
+
+    `${womanName}, twoim zadaniem jest doprowadzenie do dwóch wytrysków twojego Partnera w ciągu 25 minut. Każdy sposób jest dozwolony( kochanie się, handjob itp.). Postaraj się.`,
+
+    `${womanName}, zrób swojemu Partnerowi loda i pozwól mu się spuścić w Twoich ustach.`,
+
+    `${manName} połóż się wgyodnie na łóżku i pozwól by twoja prtnerka związała Ci ręce. ${womanName}, wejdź na jego Pyotnika. Twoim zadaniem jest jego ujeżdżanie tak by doszedł w przeciągu 4 minut. <img src=\"img/pictureW63.png\"/>`,
+
+    `${manName}, usiadź wygodnie na łóżku bądź krześle i weź w ręke telefon. ${womanName}, twoim zadaniem jest zrobienie mu 3 minutowego loda, podczas gdy On będzie Cie nagrywał. ${manName}, możesz dojśc, ale jeżeli chcesz się kochac po tym zadaniu to nie musisz dochodzić.`
+]
+
+const tasksMan = []
+tasksMan[0] = [
+    `${manName}, jesteś striptizerem, a ta dziewczyna obok cię wynajeła. Masz zrobić jej seksowny striptiz. Kuś ją, podniecaj i w czasie striptizu rozbierz ją do samej bielizny. ${womanName}, niestety nie możesz dotykać tego seksownego Stripizera, On przyjmuję inicjatywe. ${manName} Po wykonaniu zadania, ubierz się.`,
+
+    `${manName}, każ Partnerce się położyć. Następnie dotykaj i całuj Ją oraz powoli sciągaj jej ciuszki aż do samej bielizny.`,
+
+    `${manName}, zdejmij ciuszki, które Partnerka ma na sobie, zostawiając tylko bielizne. Po każdeym zdjętym ciuszku wycałuj dokladnie tą część ciała na któym był ciuszek.`,
+    `${manName}, przytulaj się do Partnerki, całuj ją i jednocześnie zdejmuj ciuszki, które ma na sobie do samej bielizny.`,
+
+    `${manName}, zasłoń sam sobie oczy lub każ zrobić to partnerce. Następnie z zasłoniętymi oczami zdejmij ubrania z Partnerki. ${womanName} ma zostać w samej bieliźnie.`,
+
+    `${manName}, seksownie zdejmij wszystkie ciuszki z Partnerki za pomocą ust. Zostaw tylko bielizne.`,
+
+]
+tasksMan[1] = [
+    `${manName}, połóż się obok swojej partnerki i szepcz jej do ucha co chciałbyś z nią teraz zrobić, możesz przy tym delikatnie masować jej ciałko gdzie tylko ze chcesz.`,
+
+    `${manName} niech partnerka usiądzie plecami do ciebie a ty zajmij się jej biustem jakbyś robił masaż, przy okazji całuj ja namiętnie po karku`,
+
+    `${manName} niech partnerka polozy się na brzuchu, a ty delikatnie połóż się na jej ciało, nastepnie użyj swojego sprzętu i ocieraj się o jej pośladki jednocześnie możesz ją całować lub szeptac jej do ucha tak by poniosła ja wyobraźnia <3`,
+
+    `${manName}, ${womanName}, połóżcie się razem blisko siebie. ${manName} twoim zadaniem będzie podniecic partnerkę dotykając ja w miejscu w którym sobie zażyczy. Pamiętajcie, że jesteście ciągle w bieliźnie.`,
+
+    `${manName}, zdejmij delikatnie majtki i połóż  się lub wypnij dupcie, a następnie poproś Partnerke o kilka klapsów na goły tyłek.`,
+
+    `${manName} niech partnerka położy się na brzuchu, a Ty swoimi ustami całuj Ją po karku i plecach przez kilka minut.`,
+
+    `${manName} wycałuj namiętnie stópki i nóżki swojej Królowej.`,
+
+    `${womanName} jest seksowną Boginią! A takiej Bogini się należy odrobina przyjemności. ${manName} każ jej się położyć a następnie namiętnie wycałuj każdy milimetr jej cudownego ciałka jednoczęsnie dotykając ją w miejsca intymne rozkoszując się ciałem Bogini. Pamiętajcie, że ${womanName} jest ciągle w bieliźnie.`,
+
+]
+tasksMan[2] = [
+    `${manName}, niech twoja partnerka polozy się wygodnie a ty za pomocą swoich raczek daj jej rozkosz.`,
+
+    `${manName}, całuj i czule dotykaj piersi Twojej Partnerki i jednocześnie pieśc Jej muszelke przez 3 minuty.`,
+
+    `${manName}, pozwól by Twoja Bogini się odprężyłą i zrelaksowała. Niech się położy a Ty zrób 5 minutowy masaż swojej Bogini.`,
+
+    `${manName}, niech Twoja Partnerka połóży się wygodnie na plecach, odsłoń przed Bartkiem swoją muszelkę i chwyć dłońmi swoje kostki. Bartek, masuj na początku bardzo delikatnie, potem coraz mocniej okrężnymi ruchami łechtaczkę Agi, ale możesz używać jedynie swojego języka.`,
+
+    `${manName}, poproś Partnerke żeby położyła się na podłodze i znacznie wypięła pośladki, zabawisz się nimi. Odsłoń je aby były nagie. Całuj, liż i przygryzaj je namiętnie, jednocześnie rękoma baw się jej szparką.`,
+
+    `${manName}, to zadanie będzie polegało na twojej uległości. Poproś Partnerke by zasłoniła Ci chustą oczy. Następnie klękniesz przed swoją Boginią. ${womanName} spuści majtki do kolan(jeżeli je ma na sobie), następnie wplecie dłonie w Twoje włosy i przyciągnie Cię do swojej doliny, będzie Ci mówić co masz robić! Pieść tak jak będzie tego chciała, rób wszystko co każe Ci zrobić.`,
+
+]
+
+tasksMan[3] = [
+    ` ${manName}, właśnie jesteś na rozmowie w sprawie pracy. Za wszelką cene musisz mieć tą prace. ${womanName}, to Twoja przyszła Szefowa. ${manName}, zrób wszystko co w Twojej mocy by dostać tą prace. Jeżeli o coś poprosi cię Twoja przyszła Szefowa to musisz to spełnić. Czas przeznaczony na rozmowe to 5 minut.`,
+
+    `${manName}, zapozuj dla swojej Partnerki. ${womanName}, zrób mu kilka fotek telefonem lub aparatem.`,
+
+    `${manName} , idź w widoczne miejsce, gdzie ktoś może Cie zobaczyć (przed okno z odsłoniętymi żaluzjami, balkon itp) i postój tam 15 sekund.`,
+
+    `${manName}, każ swojej Partnerce wstać i obrócić się do Ciebie plecami. Ty natomiast usiądź plecami do niej w jej rozkroku i zrób jej dobrze ustami przez 3 minuty. Musicie być w pozycji tak jak na poniższym obrazku. <img src=\"img/pictureM43.jpg\"/>`,
+    `
+${manName}, połóż się wygodnie na łóżku. Natomiast ty ${womanName} przyjmij pozycję jak na obrazu poniżej. ${manName} zrób jej dobrze językiem przez 2 minuty.<img src=\"img/pictureM44.jpg\"/> `,
+
+    `${manName}, połóż się wygodnie na łóżku. Natomiast ty ${womanName} przyjmij pozycję jak na obrazu poniżej. ${manName} zrób jej dobrze językiem przez 2 minuty. <img src=\"img/pictureM45.jpg\"/>`,
+
+    `${manName}, połóż się wygodnie na łóżku. Natomiast ty ${womanName} przyjmij pozycję jak na obrazu poniżej. ${manName} zrób jej dobrze językiem przez 2 minuty.<img src=\"img/pictureM46.jpg\"/>`,
+
+]
+tasksMan[4] = [
+    `${manName}, połóż się wygodnie na łóżku. ${womanName}, zwiąż Partnerowi nogi i ręcę lub przywiąż je do czegość, a następnie rób z nim co tylko chcesz przez 3 minuty. Jest do twojej dyspozycji. ${manName}, pamiętaj, że nie możesz dojść.`,
+
+    `Wejdźcie na strone internetową z kamerkami na żywo (np. showup.tv). Możecie tak ustawić kamerke by nie było widać waszych twarzy. Nastepnie ${manName} ustawi się do kamerki i pokaże swojego pytona przynajmniej kilku widzom.`,
+
+    `${womanName}, wez telefon w ręce i połóż się wygodnie na plecach na łóżku. ${manName}, musisz Ją najlepiej zadowolić swoim języczkiem. Przyjmij pozycję tak jak na poniższym zdjęciu i działaj. Masz na to 3 minuty. ${womanName}, gdy Twój Partner będzie robił Ci dobrze języczkiem, możesz go nagrywać telefonem. <img src=\"img/pictureM52.jpg\"/>`,
+
+    ` ${womanName},  połóż się wygodnie na plecach na łóżku. ${manName}, twoim zadaniem jest zrobienie eksluzywnego erotycznego masażu dla Partnerki. Zrób nastrój, zapal świece, weż oliwe do masażu i daj Jej przyjemność. Pamietaj, że to erotyczny masaż, dotykaja Ją w miejsca intymne. Czas masażu 7 minut. <img src=\"img/pictureM53.jpg\"/>`,
+
+    `${manName}, usiądź wygodnie na krześle. ${womanName}, związ mu ręce, a następnie wykorzystaj go(lub jego pytonika) jak tylko chcesz. Masz na to 5 minut.`,
+
+    `${manName}, pozwól Partnerce zrobić Tobie erotyczne zdjęcie(nudeska), możesz leciutko zasłonić swoje miejsce intymne. ${womanName}, zachowaj zdjęcie dla siebie.`,
+
+]
+tasksMan[5] = [
+    `${manName}, twoje zadanie jest proste. Doprowadź do orgazmu swojej Partnerki. Masz na to 12 minut.`,
+
+    `${manName}, jesteś erotycznym sługą swojej Partnerki. Jesteś dla jej dysopyzcji i wykonasz wszystko to o co cię poprosi. Jeżeli odmówisz wykonania polecenia to zadanie zostanie niewykonane. ${womanName}, dobrze go wykorzystaj. Możesz zażadąć twoją ulubioną pozycje lub inne zadania. Twoj niewolnik jest do 8 minutowej dyspozycji.`,
+
+    `${manName}, zacznij się kochać z swoją Partnerka w wybranej przez niąpozycji. Czas: 15 minut. Jeżeli dojdziesz przed czasem zadanie zostanie niewykonane.`,
+
+    `${manName}, zacznij się kochać z swoją Partnerka w pozycji jak na poniższym obrazku. Czas: 5 minut. Jeżeli dojdziesz przed czasem zadanie zostanie niewykonane. Nie możesz robić przerwy w trakcie stosunku. <img src=\"img/pictureM63.png\"/>`,
+
+    `${manName}, zacznij się kochać z swoją Partnerka w pozycji jak na poniższym obrazku. Czas: 5 minut. Jeżeli dojdziesz przed czasem zadanie zostanie niewykonane. Nie możesz robić przerwy w trakcie stosunku. <img src=\"img/pictureM64.png\"/>`,
+
+    `${manName}, zacznij się kochać z swoją Partnerka w pozycji jak na poniższym obrazku. Czas: 5 minut. Jeżeli dojdziesz przed czasem zadanie zostanie niewykonane. Nie możesz robić przerwy w trakcie stosunku. <img src=\"img/pictureM65.png\"/>`,
+
+    `${manName}, zacznij się kochać z swoją Partnerka w pozycji jak na poniższym obrazku. Czas: 5 minut. Jeżeli dojdziesz przed czasem zadanie zostanie niewykonane. Nie możesz robić przerwy w trakcie stosunku. <img src=\"img/pictureM66.png\"/>`,
+
+    `${manName}, zacznij się kochać z swoją Partnerka w pozycji jak na poniższym obrazku. Czas: 5 minut. Jeżeli dojdziesz przed czasem zadanie zostanie niewykonane. Nie możesz robić przerwy w trakcie stosunku. <img src=\"img/pictureM67.png\"/>`,
+
+    `${manName}, zacznij się kochać z swoją Partnerka w pozycji jak na poniższym obrazku. Czas: 5 minut. Jeżeli dojdziesz przed czasem zadanie zostanie niewykonane. Nie możesz robić przerwy w trakcie stosunku. <img src=\"img/pictureM68.png\"/>`,
+
+]
